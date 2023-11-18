aebc9c366c961935a62dfce401cbe538ced55afc
xinrachel30 <xlin30@stuy.edu>
2023-11-18 17:33:03 -0500

    initializing

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Smart-Wardrobe
+# Smart Wardrobe
+
+## Finding the most comfortable outfit regardless of the weather for you
+
+- [ ] screenshots
+
+### Description
+Smart Wardrobe is a space to track the clothing items and accessories you own. We will determine what subcategories of clothing are best suited for the weather. 
+
+- [ ] how we addressed the requirements
---
+++ backend/app.py
@@ -0,0 +1,29 @@
+import json
+from db import db
+from flask import Flask, request
+
+from db import Course
+from db import User
+from db import Assignment
+
+
+db_filename = "cms.db"
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# generalized response formats
+def success_response(data, code=200):
+    return json.dumps(data), code
+
+def failure_response(message, code=404):
+    return json.dumps({"error": message}), code
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
