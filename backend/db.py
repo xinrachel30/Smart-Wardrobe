a35e34509b7a5766ec4e39195996fc46ec2be7a8
Yilun Yin <75820637+Clearlove923@users.noreply.github.com>
2023-11-19 17:43:19 -0500

    Initialized models

--- backend/db.py
+++ backend/db.py
@@ -1,4 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
-import datetime
+from datetime import datetime
 
 db = SQLAlchemy()
+
+class Wardrobe(db.Model):
+    __tablename__ = 'wardrobe'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    type_id = db.Column(db.Integer, db.ForeignKey('type.id'), nullable=False)
+    description = db.Column(db.String(128))
+
+    type_rel = db.relationship('Type', back_populates='wardrobe_items')
+
+class Type(db.Model):
+    __tablename__ = 'type'
+    id = db.Column(db.Integer, primary_key=True)
+    style = db.Column(db.String(64), nullable=False)
+    category = db.Column(db.String(64), nullable=False)  # Assuming 'Top', 'Bottom', 'Shoe' are the only categories
+    position = db.Column(db.String(64), nullable=False)  # Assuming 'Inner', 'Middle', 'Outer' are the only positions
+    cold_resistance = db.Column(db.Integer, nullable=False)  # Assuming this is an integer between 1 and 4
+
+    wardrobe_items = db.relationship('Wardrobe', order_by='Wardrobe.id', back_populates='type_rel')
+
+
