729505b9385a2b7892b8d7d78222ac0de330f39c
Yilun Yin <75820637+Clearlove923@users.noreply.github.com>
2023-11-19 17:45:14 -0500

    Added descriptions

--- backend/db.py
+++ backend/db.py
@@ -4,6 +4,10 @@ from datetime import datetime
 db = SQLAlchemy()
 
 class Wardrobe(db.Model):
+    """
+    Id	Name(Inputed by user)	Type(Related to Type, chosen by user)	Description(Inputed by user)	
+1	Red_Sweater	1	/	/	
+    """
     __tablename__ = 'wardrobe'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
@@ -13,6 +17,13 @@ class Wardrobe(db.Model):
     type_rel = db.relationship('Type', back_populates='wardrobe_items')
 
 class Type(db.Model):
+    """		
+    Id	Style	Category(Top/Bottom/Shoe)	Position(Inner/Middle/Outer)	ColdResistance(Range from 1 to 4)
+    1	sweater	Top	Middle	3
+    2	longSleeve	Top	Inner	2
+    3	shortSleeve	Top	Inner	1
+    4	jacket	Top	Outer	4
+    """
     __tablename__ = 'type'
     id = db.Column(db.Integer, primary_key=True)
     style = db.Column(db.String(64), nullable=False)
@@ -21,5 +32,6 @@ class Type(db.Model):
     cold_resistance = db.Column(db.Integer, nullable=False)  # Assuming this is an integer between 1 and 4
 
     wardrobe_items = db.relationship('Wardrobe', order_by='Wardrobe.id', back_populates='type_rel')
+
 
 
