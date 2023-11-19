6d1b41853effffc0e3e2c35bd22e5ec9512ea8f6
Yilun Yin <75820637+Clearlove923@users.noreply.github.com>
2023-11-19 18:06:25 -0500

    Edited models

--- backend/db.py
+++ backend/db.py
@@ -3,35 +3,35 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-class Wardrobe(db.Model):
+class Item(db.Model):
     """
-    Id	Name(Inputed by user)	Type(Related to Type, chosen by user)	Description(Inputed by user)	
+    id	name(Inputed by user)	style(Related to Category, chosen by user)	description(Inputed by user)	
 1	Red_Sweater	1	/	/	
     """
-    __tablename__ = 'wardrobe'
+    __tablename__ = 'item'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
-    type_id = db.Column(db.Integer, db.ForeignKey('type.id'), nullable=False)
+    style_id = db.Column(db.Integer, db.ForeignKey('style.id'), nullable=False)
     description = db.Column(db.String(128))
 
-    type_rel = db.relationship('Type', back_populates='wardrobe_items')
+    style_rel = db.relationship('Style', back_populates='item_items')
 
-class Type(db.Model):
+class Style(db.Model):
     """		
-    Id	Style	Category(Top/Bottom/Shoe)	Position(Inner/Middle/Outer)	ColdResistance(Range from 1 to 4)
+    id	style	category(Top/Bottom/Shoe)	position(Inner/Middle/Outer)	cold_resistance(Range from 1 to 4)
     1	sweater	Top	Middle	3
     2	longSleeve	Top	Inner	2
     3	shortSleeve	Top	Inner	1
     4	jacket	Top	Outer	4
     """
-    __tablename__ = 'type'
+    __tablename__ = 'style'
     id = db.Column(db.Integer, primary_key=True)
     style = db.Column(db.String(64), nullable=False)
     category = db.Column(db.String(64), nullable=False)  # Assuming 'Top', 'Bottom', 'Shoe' are the only categories
     position = db.Column(db.String(64), nullable=False)  # Assuming 'Inner', 'Middle', 'Outer' are the only positions
     cold_resistance = db.Column(db.Integer, nullable=False)  # Assuming this is an integer between 1 and 4
 
-    wardrobe_items = db.relationship('Wardrobe', order_by='Wardrobe.id', back_populates='type_rel')
+    item_items = db.relationship('Item', order_by='Item.id', back_populates='style_rel')
 
 
 
