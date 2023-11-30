f823213aeb872264fa333c09f221e141adf87b54
Aiden Montesinos <aom33@cornell.edu>
2023-11-30 03:36:11 -0500

    cells created

--- frontend/DressingWithWeather/DressingWithWeather/ViewController.swift
+++ frontend/DressingWithWeather/DressingWithWeather/ViewController.swift
@@ -11,57 +11,67 @@ class ViewController: UIViewController {
     
     // MARK: - Properties (view)
     
-    private var tableView: UITableView {
+    private lazy var tableView: UITableView = {
         let cells = UITableView()
         cells.translatesAutoresizingMaskIntoConstraints = false
-        cells.backgroundColor = .systemMint
+        cells.backgroundColor = .orange
         cells.rowHeight = UITableView.automaticDimension
         cells.estimatedRowHeight = 44
+        cells.separatorStyle = .none
         cells.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         return cells
-    }
+    }()
     
     // MARK: - Properties (data)
     //let InputMe:UITextField
     
     // MARK: - viewDidLoad
     
+    override func loadView() {
+        super.loadView()
+        setupTableView()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "ChatDev"
         view.backgroundColor = .blue
-        setupCollectionView()
     }
-    
+}
         
         
     
     // MARK: - Set Up Views
-    
-    private func setupCollectionView() {
-        // TODO: Set Up CollectionView
-        NSLayoutConstraint.activate([
-            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            tableView.topAnchor.constraint(equalTo: view.centerYAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
+    private extension ViewController{
+        func setupTableView() {
+            // TODO: Set Up CollectionView
+            tableView.dataSource = self
+            
+            self.view.addSubview(tableView)
+            //tableView.backgroundColor = .white
+            print("subview")
+            NSLayoutConstraint.activate([
+                tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                tableView.topAnchor.constraint(equalTo: view.topAnchor),
+                tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            ])
+        }
     }
     
+    
 
 
-// MARK: - UICollectionViewDelegate
+// MARK: - UITableViewDelegate
 
 
-// MARK: - UICollectionViewDataSource
+// MARK: - UITableViewViewDataSource
 
 
 
-// MARK: - UICollectionViewDelegateFlowLayout
+// MARK: - UITableViewDelegateFlowLayout
 
-}
+
 extension ViewController:UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
@@ -69,6 +79,19 @@ extension ViewController:UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        switch indexPath.row{
+        case 0:
+            cell.backgroundColor = .white
+            print("white")
+        case 1:
+            cell.backgroundColor = .red
+        case 2:
+            cell.backgroundColor = .green
+        default:
+            print("default")
+            break
+        }
+        print("")
         return cell
     }
     
